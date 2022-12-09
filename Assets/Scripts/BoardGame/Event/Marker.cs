5ffa0954edfaf18f28e53547ceeaa9f9b77556c9
mitsuruyamaura <komayou326@gmail.com>
2022-12-09 12:55:11 +0900

    マーカー用のゲームオブジェクトを作成。プレファブ化。OwnerType.cs、Maker.cs 作成。

---
+++ Assets/Scripts/BoardGame/Event/Marker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace yamap_BoardGame {
+
+    public class Marker : MonoBehaviour {
+        public OwnerType ownerType;
+    }
+}

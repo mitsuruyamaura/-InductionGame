baa5234b03dac311f8e59c2de226b463f3efb6f9
mitsuruyamaura <komayou326@gmail.com>
2022-12-09 10:19:29 +0900

    DirectionType.cs 作成。Panel.cs、CharactorMover.cs を修正し、4方向への向きの同期処理を追加。

--- Assets/Scripts/BoardGame/CharaMove/Panel.cs
+++ Assets/Scripts/BoardGame/CharaMove/Panel.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Panel : MonoBehaviour
-{
-    private int no;
+namespace yamap_BoardGame {
 
-    // イベントの種類の enum
+    public class Panel : MonoBehaviour {
+
+        private int no;
+
+        // 次に進む向きの種類　配列で用意。１つしかなければそちらに行く。2つ以上あるときには選択肢を出す
+        public DirectionType directionType;
+
+        // イベントの種類の enum
 
 
+        // 陣地
+        public List<Marker> markerList = new();
+    }
 }
